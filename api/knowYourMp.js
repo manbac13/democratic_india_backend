83cb10e0a24359ba0e29323aa315d1f9eefbec5f
manishbachhavservicepackai <manish.b@servicepack.ai>
2024-11-14 18:53:54 +0530

    versel.json added

--- api/knowYourMp.js
+++
@@ -1,7 +0,0 @@
-// api/yourmp.js
-const app = require('../index'); // Import the main Express app
-
-module.exports = (req, res) => {
-  req.url = `/yourmp${req.url}`;  // Adjust the URL to match your route in Express
-  app(req, res);                   // Forward request to the main app
-};
