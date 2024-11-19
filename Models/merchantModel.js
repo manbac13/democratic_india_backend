6a9c9b36dc91473c024386c958fc2fe009a240b2
manishbachhavservicepackai <manish.b@servicepack.ai>
2024-11-19 18:29:44 +0530

    mongodb removed

--- Models/merchantModel.js
+++ Models/merchantModel.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/");
+// const mongoose = require("mongoose");
+// mongoose.connect("mongodb://127.0.0.1:27017/");
 
-const Schema = mongoose.Schema;
+// const Schema = mongoose.Schema;
 
-const merchantSchema = new Schema({
-  owner_name: { type: String, required: true },
-  email: { type: String, required: true },
-  entity_name: { type: String, required: true },
-  town_name: { type: String, required: true },
-  location: { type: String, required: true },
-  phone_number: { type: String, required: true },
-  category: { type: String, required: true },
-});
+// const merchantSchema = new Schema({
+//   owner_name: { type: String, required: true },
+//   email: { type: String, required: true },
+//   entity_name: { type: String, required: true },
+//   town_name: { type: String, required: true },
+//   location: { type: String, required: true },
+//   phone_number: { type: String, required: true },
+//   category: { type: String, required: true },
+// });
 
-const merchants = mongoose.model("merchants", merchantSchema);
-module.exports = merchants;
+// const merchants = mongoose.model("merchants", merchantSchema);
+// module.exports = merchants;
--- Routes/merchants.js
+++ Routes/merchants.js
@@ -1,79 +1,79 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const Merchants = require("../Models/merchantModel");
+// const express = require("express");
+// const bodyParser = require("body-parser");
+// const Merchants = require("../Models/merchantModel");
 
-const router = express.Router();
+// const router = express.Router();
 
-router.use(bodyParser());
+// router.use(bodyParser());
 
-router.get("/", async (req, res) => {
-  const allMerchants = await Merchants.find();
-  res.status(200).json({
-    success: true,
-    data: allMerchants,
-    message: "data fetched successfully!",
-  });
-});
+// router.get("/", async (req, res) => {
+//   const allMerchants = await Merchants.find();
+//   res.status(200).json({
+//     success: true,
+//     data: allMerchants,
+//     message: "data fetched successfully!",
+//   });
+// });
 
-router.post("/", async (req, res) => {
-  try {
-    const merchantData = await Merchants.create(req.body);
-    res.status(200).json({
-      success: true,
-      data: merchantData,
-      message: "merchant has been created successfully!",
-    });
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: `something went wrong while creating merchant! ${error.message}`,
-    });
-  }
-});
+// router.post("/", async (req, res) => {
+//   try {
+//     const merchantData = await Merchants.create(req.body);
+//     res.status(200).json({
+//       success: true,
+//       data: merchantData,
+//       message: "merchant has been created successfully!",
+//     });
+//   } catch (error) {
+//     res.status(400).json({
+//       success: false,
+//       message: `something went wrong while creating merchant! ${error.message}`,
+//     });
+//   }
+// });
 
-router.put("/:id", async (req, res) => {
-  try {
-    const foundMerchant = await Merchants.findOne({ _id: req.params.id });
-    if (foundMerchant) {
-      await Merchants.updateOne(foundMerchant, req.body);
-      res.status(200).json({
-        success: true,
-        message: "merchant data has been successfully updated!",
-      });
-    } else {
-      res.status(400).json({
-        success: true,
-        message: "merchant does not exist!",
-      });
-    }
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: `something went wrong while updating merchant! ${error.message}`,
-    });
-  }
-});
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const foundMerchant = await Merchants.findOne({ _id: req.params.id });
+//     if (foundMerchant) {
+//       await Merchants.updateOne(foundMerchant, req.body);
+//       res.status(200).json({
+//         success: true,
+//         message: "merchant data has been successfully updated!",
+//       });
+//     } else {
+//       res.status(400).json({
+//         success: true,
+//         message: "merchant does not exist!",
+//       });
+//     }
+//   } catch (error) {
+//     res.status(400).json({
+//       success: false,
+//       message: `something went wrong while updating merchant! ${error.message}`,
+//     });
+//   }
+// });
 
-router.delete('/:id', async(req,res)=>{
-  try {
-    const foundMerchant = await Merchants.findOne({ _id: req.params.id });
-    if (foundMerchant) {
-      await Merchants.deleteOne(foundMerchant._id);
-      res.status(200).json({
-        success: true,
-        message: "merchant data has been successfully deleted!",
-      });
-    } else {
-      res.status(400).json({
-        success: true,
-        message: "merchant does not exist!",
-      });
-    }
-  } catch (error) {
-    res.status(400).json({
-      success: false,
-      message: `something went wrong while deleting merchant! ${error.message}`,
-    });
-  }
-})
-module.exports = router;
+// router.delete('/:id', async(req,res)=>{
+//   try {
+//     const foundMerchant = await Merchants.findOne({ _id: req.params.id });
+//     if (foundMerchant) {
+//       await Merchants.deleteOne(foundMerchant._id);
+//       res.status(200).json({
+//         success: true,
+//         message: "merchant data has been successfully deleted!",
+//       });
+//     } else {
+//       res.status(400).json({
+//         success: true,
+//         message: "merchant does not exist!",
+//       });
+//     }
+//   } catch (error) {
+//     res.status(400).json({
+//       success: false,
+//       message: `something went wrong while deleting merchant! ${error.message}`,
+//     });
+//   }
+// })
+// module.exports = router;
